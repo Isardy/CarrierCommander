221a9df06658c812c6052c8f1f9bf78adb42b6ff
Isardy <dustygirardin@gmail.com>
2026-03-09 00:47:41 +0100

    Update mod metadata and add thumbnail

--- modinfo.lua
+++ modinfo.lua
@@ -1,16 +1,17 @@
 meta =
 {
-    id = "CarrierCommander",
+    id = "3677585938",
     name = "CarrierCommander",
     title = "Carrier Commander",
+    type = "mod",
     description = "Adds a new captain type: Carrier Commander.",
     authors = {"isardy"},
-    version = "1.0.0",
+    version = "1.0.1",
     dependencies = {
         {id = "Avorion"}
     },
     serverSideOnly = false,
     clientSideOnly = false,
     saveGameAltering = true,
-    contact = ""
+    contact = "Leave a comment if you have any issues or suggestions"
 }
